e4b334db3935380cf04fbe8699365b6739f7888c
hw08798300 <3401552440@qq.com>
2020-05-09 09:51:28 +0800

    解决常年值数据 和开发四季入季开始和结束时间

--- src/main/java/com/dafang/monitor/nx/statistics/service/impl/NumericalServiceImpl.java
+++ src/main/java/com/dafang/monitor/nx/statistics/service/impl/NumericalServiceImpl.java
@@ -149,7 +149,7 @@ public class NumericalServiceImpl implements NumericalService {
         int startYear = Convert.toInt(params.getStartDate().substring(0, 4));
         int endYear = Convert.toInt(params.getEndDate().substring(0, 4));
         String[] scales = params.getClimateScale().split("-");// 常年值区间段
-        int scaleLen = Convert.toInt(scales[1]) -  Convert.toInt(scales[0]);
+        int scaleLen = Convert.toInt(scales[1]) -  Convert.toInt(scales[0]) + 1;
         // 获取数据
         List<Daily> periodList = mapper.periodList(params);
         List<String> staList = periodList.parallelStream().map(x -> x.getStationNo()).distinct().collect(Collectors.toList());
